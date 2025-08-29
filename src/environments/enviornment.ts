ab8d6d1e1fbfdc1595e908967c9a38cdc73cf7d4
MaxLyonel <leorexx100@gmail.com>
2025-08-29 09:49:52 -0400

    wip: cambiando variables de environment

--- src/environments/enviornment.ts
+++ src/environments/enviornment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://172.20.196.69:3000/api'
+  // apiUrl: 'http://172.20.196.69:3000/api'
+  apiUrl: 'https://api.altademanda.test.sie.gob.bo/'
 };
