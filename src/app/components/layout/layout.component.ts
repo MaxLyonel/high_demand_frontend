1e4b6b3b362a2989c896505a332a73fc62e4b94c
MaxLyonel <leorexx100@gmail.com>
2025-08-29 15:22:18 -0400

    feat: diseño UI de la pagina principal

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from "@angular/core";
+import { Component, ViewEncapsulation, AfterViewInit, OnInit } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { NzButtonModule } from "ng-zorro-antd/button";
 import { NzCardModule } from "ng-zorro-antd/card";
@@ -26,4 +26,48 @@ import { LoginComponent } from "./components/login/login.component";
   styleUrls: ['./layout.component.less'],
   encapsulation: ViewEncapsulation.None
 })
-export default class LayoutComponent {}
+export default class LayoutComponent implements OnInit {
+  // AfterViewInit
+  // ngAfterViewInit() {
+  //   this.createParticles(100);
+  // }
+  ngOnInit() {
+    this.createParticles(200);
+  }
+
+  createParticles(count: number) {
+    const background = document.getElementById('background');
+    const symbols = ['★', '✪', '✶', '✷', '✸', '✹', '✺'];
+
+    // 👉 Selecciona un símbolo UNA sola vez
+    const chosenSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+
+    for (let i = 0; i < count; i++) {
+      const particle = document.createElement('div');
+      particle.className = 'code-particle';
+      // Todas las partículas usan el mismo símbolo
+      // particle.textContent = chosenSymbol.repeat(Math.floor(Math.random() * 3) + 1);
+      particle.textContent = chosenSymbol;
+      // Posición aleatoria
+      const left = Math.random() * 100;
+      particle.style.left = `${left}%`;
+      // Tamaño aleatorio
+      const size = 15 + Math.random() * 10;
+      particle.style.fontSize = `${size}px`;
+      // Opacidad aleatoria
+      const opacity = 0.2 + Math.random() * 0.3;
+      particle.style.opacity = opacity.toString();
+      // Retraso de animación aleatorio
+      const delay = Math.random() * 20;
+      particle.style.animationDelay = `${delay}s`;
+      // Duración de animación aleatoria
+      const duration = 12 + Math.random() * 1;
+      particle.style.animationDuration = `${duration}s`;
+
+      if (background) {
+        background.appendChild(particle);
+      }
+    }
+  }
+
+}
