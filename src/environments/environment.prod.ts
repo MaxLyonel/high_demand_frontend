c579b67858961380c57390a061e83683d204114f
MaxLyonel <leorexx100@gmail.com>
2025-08-29 10:52:26 -0400

    wip: agregando api a la ruta del backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // apiUrl: 'http://172.20.196.69:3000/api'
-  apiUrl: 'https://api.altademanda.test.sie.gob.bo/'
+  apiUrl: 'https://api.altademanda.test.sie.gob.bo/api'
 };
