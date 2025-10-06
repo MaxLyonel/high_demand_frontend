ab681e24016c402aaedcbaebe1128f9130ed4b62
MaxLyonel <leorexx100@gmail.com>
2025-10-06 08:48:32 -0400

    wip: Ignorar carpeta src/environment

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://api.altademanda.test.sie.gob.bo/api'
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://api.altademanda.test.sie.gob.bo/api'
-};
